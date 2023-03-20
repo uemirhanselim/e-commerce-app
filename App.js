1525a8250ddc6eeb29e76167cd9575b3225b2168
uemirhanselim <uemirhanselim@gmail.com>
2023-03-20 21:43:03 +0300

    rating component has been created

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
 import StackNavigator from './src/navigator/StackNavigator';
 
 export default function App() {
   return (
-    <StackNavigator></StackNavigator>
+    <NativeBaseProvider>
+      <StackNavigator></StackNavigator>
+    </NativeBaseProvider>
   );
 }
 
