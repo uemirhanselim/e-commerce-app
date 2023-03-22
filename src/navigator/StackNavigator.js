fadf467c2b3cc6b94f764c75443d461f1e21582d
uemirhanselim <uemirhanselim@gmail.com>
2023-03-22 22:45:24 +0300

    nav added

--- src/navigator/StackNavigator.js
+++ src/navigator/StackNavigator.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import WelcomeScreen from '../screens/WelcomeScreen';
 import TabNavigator from './TabNavigator';
+import ProductDetailsScreen from '../screens/ProductDetailsScreen';
+import CardScreen from '../screens/CardScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +19,8 @@ const StackNavigator = () => {
       >
         <Stack.Screen name='Welcome' component={WelcomeScreen} />
         <Stack.Screen name='TabNavigator' component={TabNavigator} />
+        <Stack.Screen name='ProductDetailsScreen' component={ProductDetailsScreen} />
+        <Stack.Screen name='CardScreen' component={CardScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
