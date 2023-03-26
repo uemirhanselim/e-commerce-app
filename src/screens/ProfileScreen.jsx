1a517394f04e1cf7b68249722ab478f76d1aa007
uemirhanselim <uemirhanselim@gmail.com>
2023-03-26 10:52:48 +0300

    profile screen username update added

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,11 +1,87 @@
-import { View, Text } from 'react-native'
+
+import { Box, Center, FormControl, Heading, Image, Input, ScrollView, Text, VStack } from 'native-base'
 import React from 'react'
+import { useState } from 'react'
+import CustomButton from '../components/CustomButton'
+import Colors from '../constants/Colors'
+
+const Inputs = [
+    {
+        label: "USERNAME",
+        type: "text",
+    },
+    {
+        label: "EMAIL",
+        type: "text",
+    },
+    {
+        label: "NEW PASSWORD",
+        type: "password",
+    },
+    {
+        label: "CONFIRM PASSWORD",
+        type: "password",
+    },
+]
 
 const ProfileScreen = () => {
+    const [displayedUsername, setDisplayedUsername] = useState("Williams Robin")
+    const [usernameController, setUsernameController] = useState("")
     return (
-        <View>
-            <Text>ProfileScreen</Text>
-        </View>
+        <>
+            <Center bg={Colors.orange} pt={10} pb={6}>
+                <Image source={{ uri: 'https://i1.sndcdn.com/avatars-000583246488-dhm5la-t500x500.jpg' }}
+                    alt='profile'
+                    w={108}
+                    h={108}
+                    resizeMode='cover'
+                    borderRadius={60}
+                />
+                <Heading bold fontSize={15} isTruncated my={2} color={Colors.white}>
+                    {displayedUsername}
+                </Heading>
+                <Text italic fontSize={10} color={Colors.white}>
+                    Joined in March 21 2023
+                </Text>
+            </Center>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Box h='full' bg={Colors.white} px={5}>
+                    <VStack space={10} mt={5} pb={10}>
+                        {
+                            Inputs.map((item, index) => (
+                                <FormControl key={index}>
+                                    <FormControl.Label
+                                        _text={{
+                                            fontSize: "12px",
+                                            fontWeight: "bold",
+                                        }}
+                                    >
+                                        {item.label}
+                                    </FormControl.Label>
+                                    <Input
+                                        borderWidth={0.2}
+                                        bg={Colors.lightOrange}
+                                        py={4}
+                                        type={item.type}
+                                        color={Colors.black}
+                                        fontSize={15}
+                                        _focus={{ bg: Colors.lightOrange, borderWidth: 1, }}
+                                        onChangeText={(value) => setUsernameController(value)}
+                                        value={item.label === "USERNAME" ? usernameController : null}
+                                    />
+                                </FormControl>
+                            ))
+                        }
+                        <CustomButton bg={Colors.orange} color={Colors.white} onPress={() => {
+                            setDisplayedUsername(usernameController)
+                            setUsernameController("")
+                        }}>
+                            UPDATE PROFILE
+                        </CustomButton>
+                    </VStack>
+                </Box>
+            </ScrollView>
+        </>
     )
 }
 
