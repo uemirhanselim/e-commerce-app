9448a82d9b55a84c3717f7015159946a4dd12b20
uemirhanselim <uemirhanselim@gmail.com>
2023-03-26 10:53:48 +0300

    tab navigator

--- src/navigator/TabNavigator.js
+++ src/navigator/TabNavigator.js
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import { Icon } from '@rneui/themed';
+import Colors from '../constants/Colors';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -11,14 +11,22 @@ const TabNavigator = () => {
     return (
         <Tab.Navigator
             initialRouteName='Home'
+            barStyle={{ height: 60, backgroundColor: Colors.orange, }}
+            activeColor={Colors.black}
+            screenOptions={{
+                tabBarLabel: ''
+
+            }}
+
         >
             <Tab.Screen name='Home' component={HomeScreen} options={{
                 tabBarIcon: ({ size, color }) => (
                     <Icon name='home' type='entypo' />
-                )
+                ),
+
             }} />
             <Tab.Screen name='Profile' component={ProfileScreen} options={{
-                 tabBarIcon: ({ size, color }) => (
+                tabBarIcon: ({ size, color }) => (
                     <Icon name='man' type='entypo' />
                 )
             }} />
