2b29fbe59aab65cd607cea59416f916509d36cb3
uemirhanselim <uemirhanselim@gmail.com>
2023-03-26 10:54:58 +0300

    card components has done

--- src/components/CardEmpty.js
+++ src/components/CardEmpty.js
@@ -4,8 +4,10 @@ import React from 'react'
 import Colors from '../constants/Colors'
 import { FontAwesome } from '@expo/vector-icons'
 import CustomButton from './CustomButton'
+import { useNavigation } from '@react-navigation/native'
 
 const CardEmpty = () => {
+    const navigation = useNavigation()
     return (
         <Box flex={1} px={4}>
             <Center h="90%">
@@ -14,7 +16,7 @@ const CardEmpty = () => {
                 </Center>
                 <Text color={Colors.orange} bold mt={5}>CARD IS EMPTY</Text>
             </Center>
-            <CustomButton bg={Colors.black} color={Colors.white}>
+            <CustomButton bg={Colors.black} color={Colors.white} onPress={() => navigation.goBack()}>
                 START SHOPPING
             </CustomButton>
         </Box>
--- src/components/CardItems.js
+++ src/components/CardItems.js
@@ -1,60 +1,78 @@
 
-import { Box, Button, Center, HStack, Image, Pressable, Text, VStack } from 'native-base'
+import { Box, Button, Center, HStack, Image, Pressable, Text, View, VStack } from 'native-base'
 import React from 'react'
-import { SwipeListView } from 'react-native-swipe-list-view'
 import Colors from '../constants/Colors'
 import { FontAwesome } from '@expo/vector-icons'
+import { SwipeListView } from 'react-native-swipe-list-view'
+import { useDispatch } from 'react-redux'
+import { DeleteFromCard } from '../redux/actions/cardActions'
 
-const Swiper = () => {
-    <SwipeListView
-        rightOpenValue={-50}
-        previewRowKey="0"
-        previewOpenValue={-40}
-        previewOpenDelay={3000}
-        data={prdoucts.slice(0.2)} // TODO: CHange here later
-        renderHiddenItem={renderHiddenItems}
-        renderItem={renderItems} />
-
+const Swiper = ({ card }) => {
+    return (
+        <View>
+            <SwipeListView
+                rightOpenValue={-50}
+                previewRowKey="0"
+                previewOpenValue={-40}
+                previewOpenDelay={3000}
+                data={card}
+                renderItem={renderItems}
+                renderHiddenItem={renderHiddenItems}
+            />
+        </View>
+    );
 }
 
-const renderItems = (data) => {
-    <Pressable>
-        <Box ml={6} mb={3}>
-            <HStack alignItems="center" bg={Colors.white} shadow={1} rounded={10} overflow='hidden'>
-                <Center w='25%' bg={Colors.lightBlack}>
-                    <Image source={{
-                        uri: data.item.url //TODO change this 
-                    }} alt={data.item.name} w='full' resizeMode='contain'
-                    />
-                </Center>
-                <VStack w='60%' px={2} space={2}>
-                    <Text isTruncated color={Colors.black} bold fontSize={10}>
-                        {data.item.name} //TODO Change here
-                    </Text>
-                    <Text bold color={Colors.lightBlack}>${price}</Text>//TODO: Change here
-                </VStack>
-                <Center>
-                    <Button bg={Colors.orange} _pressed={{ bg: Colors.orange }} _text={{
-                        color:Colors.white
-                    }}>5</Button>
-                </Center>
-            </HStack>
-        </Box>
-    </Pressable>
+
+const renderItems = ({ item }) => {
+
+    return (
+        <Pressable>
+            <Box ml={6} mb={3} s>
+                <HStack alignItems="center" bg={Colors.white} shadow={1} rounded={10} h={20} overflow='hidden'>
+                    <Center w='25%' h={10} bg={Colors.lightBlack}>
+                        <Image source={{
+                            uri: item['thumbnail']
+                        }} alt={item['title']} w='full' h={20} resizeMode='contain'
+                        />
+                    </Center>
+                    <VStack pl={5} w='60%' px={2} space={2}>
+                        <Text isTruncated color={Colors.black} bold fontSize={16}>
+                            {item['title']}
+                        </Text>
+                        <Text bold color={Colors.lightBlack}>${item['price']}</Text>
+                    </VStack>
+                    {/* <Center>
+                        <Button bg={Colors.orange} _pressed={{ bg: Colors.orange }} _text={{
+                            color: Colors.white
+                        }}>1</Button>
+                    </Center> */}
+                </HStack>
+            </Box>
+        </Pressable >
+    );
+};
+
+
+const renderHiddenItems = ({ item }) => {
+    return (
+        <Pressable w={60} roundedTopRight={10} roundedBottomRight={10} h={20} ml='auto' justifyContent="center" bg={Colors.red}>
+            <Center alignItems="center" >
+                <FontAwesome name='trash' size={24} color={Colors.white} />
+            </Center>
+        </Pressable>
+    );
 }
 
-const renderHiddenItems = () => {
-    <Pressable w={50} roundedTopRight={10} roundedBottomRight={10} h='88%' ml='auto' justifyContent="center" bg={Colors.red}>
-        <Center alignItems="center" >
-            <FontAwesome name='trash' size={24} color={Colors.white} />
-        </Center>
-    </Pressable>
-}
 
-const CardItems = () => {
+const CardItems = ({ card }) => {
+    const dispatch = useDispatch()
+    const deleteItem = (id) => {
+        dispatch(DeleteFromCard(id))
+    }
     return (
         <Box mr={6}>
-            <Swiper />
+            <Swiper card={card} />
         </Box>
 
     )
