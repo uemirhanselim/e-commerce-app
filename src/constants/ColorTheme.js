dd913c48e71df4f01796f361c89083f3f6fa8304
uemirhanselim <uemirhanselim@gmail.com>
2023-06-19 10:45:35 +0300

    theme colors have been added

---
+++ src/constants/ColorTheme.js
@@ -0,0 +1,15 @@
+const DarkTheme = {
+    item: 'rgb(130, 148, 196)',
+    item1: 'rgb(172, 177, 214)',
+    item2: 'rgb(219, 223, 234)',
+    background: 'rgb(255, 234, 210)',
+}
+
+const LightTheme = {
+    item: 'rgb(47, 143, 157)',
+    item1: 'rgb(59, 172, 182)',
+    item2: 'rgb(130, 219, 216)',
+    background: 'rgb(179, 232, 229)',
+}
+
+export {LightTheme, DarkTheme}
