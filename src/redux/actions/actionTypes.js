77863c9d5bd43b74e33c6e5c0cb46ffd2c0deb9f
uemirhanselim <uemirhanselim@gmail.com>
2023-06-19 10:46:29 +0300

    new action types have been created

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,3 +1,6 @@
 export const ADD_TO_CARD = "ADD_TO_CARD"
 export const DELETE_FROM_CARD = "DELETE_FROM_CARD"
 export const UPDATE_URI = "UPDATE_URI"
+export const SET_VISITE = "SET_VISITE"
+export const SET_ORDER = "SET_ORDER"
+export const SET_PAYMENT = "SET_PAYMENT"
