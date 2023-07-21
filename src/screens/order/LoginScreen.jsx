8b55cdf7e7593a883da06559eca17efec57a8a3f
uemirhanselim <uemirhanselim@gmail.com>
2023-07-21 17:02:54 +0300

    login screen few changes

--- src/screens/order/LoginScreen.jsx
+++ src/screens/order/LoginScreen.jsx
@@ -5,20 +5,29 @@ import LoginButton from '../../components/LoginButton'
 import { LoginEmailField, LoginPassField } from '../../components/LoginTextFields'
 import { Box } from 'native-base'
 import SignIn from '../../service/AuthService'
-import axios from 'axios'
+import { useNavigation } from '@react-navigation/native'
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState("")
+    const navigation = useNavigation()
+    const [email, setEmail] = useState("")  
     const [password, setPassword] = useState("")
+
+    const _onPress = async () => {
+        const response = await SignIn({ email: email, password: password })
+        if (response.status === 200) {
+            navigation.navigate("HomeScreenT", response)
+        } else if (response.status === 401) {
+            
+        }
+    }
+
     return (
         <View style={style.container}>
-            <LoginEmailField />
+            <LoginEmailField email={email} setEmail={setEmail} />
             <Box height={4} />
-            <LoginPassField />
+            <LoginPassField password={password} setPassword={setPassword}/>
             <Box height={30} />
-            <LoginButton onPress={async () => {
-                await SignIn({ email: "admin@demo", password: "demo" })
-            }} />
+            <LoginButton onPress={_onPress} isActive={email !== "" && password !== ""}/>
         </View>
     )
 }
