72c0037f74ee13df29b364ed3ff940e23efe3ae3
uemirhanselim <uemirhanselim@gmail.com>
2023-07-24 18:00:52 +0300

    home screen ui

--- src/screens/order/HomeScreen.jsx
+++ src/screens/order/HomeScreen.jsx
@@ -1,12 +1,88 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, Dimensions } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native'
+import { Box, FlatList, Icon, ScrollView } from 'native-base';
+import { Ionicons } from '@expo/vector-icons'
 
-const HomeScreenT = ({data}) => {
+// Gap stuff
+const { width, height } = Dimensions.get('window');
+
+const renderItem = ({ item }) => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={style.item}>
+      <Icon as={Ionicons} name={item['icon']} size={60} color="white" />
+      <Text style={{ color: 'white', fontSize: 15, fontWeight: '500' }}>{item['title']}</Text>
     </View>
   )
+};
+const HomeScreenT = () => {
+  const route = useRoute()
+  const data = route.params.item
+
+  const homeData = [
+    {
+      "title": "Zaman Çizelgesi",
+      "icon": "time",
+    }
+  ]
+  return (
+    <SafeAreaView style={style.container}>
+      <View style={style.appBar}>
+        <Text style={style.text}>
+          Duyu
+          <Text style={style.textSpan}>Box</Text>
+        </Text>
+        <Text style={{ fontSize: 15, fontWeight: '500' }}>{data['customer-name']}</Text>
+      </View>
+      <View style={{
+        flex: 1, justifyContent: 'center', alignItems: 'flex-start', paddingHorizontal: 10
+      }}>
+        <FlatList
+          numColumns={2}
+          data={homeData}
+          renderItem={renderItem}
+        />
+      </View>
+    </SafeAreaView>
+  )
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  textSpan: {
+    color: 'rgb(110,0,239)'
+  },
+  appBar: {
+    backgroundColor: 'white',
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    height: '10%',
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+  },
+  item: {
+    backgroundColor: 'rgba(110,0,239, 0.5)',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+    marginHorizontal: 4,
+    width: width / 2.3,
+    height: height / 5,
+    borderRadius: 8,
+    paddingVertical: 30,
+  }
+})
 
 export default HomeScreenT
